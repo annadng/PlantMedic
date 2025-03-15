1430860734791ebeb7196a4aad34333835bde286
cooper <cedw0016@student.monash.edu>
2025-03-15 23:05:29 +1100

    Airtable connected i think

--- Airtable/airtable.js
+++ Airtable/airtable.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const AIRTABLE_PAT = process.env.REACT_APP_AIRTABLE_PAT;  // Replace with your actual token
+const AIRTABLE_BASE_ID = "appK4c7AaDouGVGbz"
 
 const airtableAPI = axios.create({
-  baseURL: `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}`,
+  baseURL: `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${"Plants"}`,
   headers: {
     Authorization: `Bearer ${AIRTABLE_PAT}`,
     "Content-Type": "application/json",
