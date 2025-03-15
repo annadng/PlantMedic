0222b93264309ec6adc6e0330682f29ed486ec78
Anna Dang <annadang2001@gmail.com>
2025-03-16 09:06:46 +1100

    Set up React Router to allow routing between pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main } from "./screens/Main";
+import { Diagnosis } from "./screens/Diagnosis"; 
+// import CareRecommendations from "./pages/CareRecommendations"; 
+import { History } from "./screens/History"; 
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <Main />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={< Main />} />
+        <Route path="/instant-diagnosis" element={< Diagnosis />} />
+        {/* <Route path="/care-recommendations" element={< CareRecommendations />} /> */}
+        <Route path="/history" element={< History />} />
+      </Routes>
+    </Router>
+  </StrictMode>
 );
